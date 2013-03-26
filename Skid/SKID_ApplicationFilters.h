87d9357190283218d9b77f890ffeffc49043fdd6
Brad Allred <bradallred@me.com>
2013-03-26 13:09:13 -0600

    Update file preambe to GPLv2

--- Skid/SKID_ApplicationFilters.h
+++ Skid/SKID_ApplicationFilters.h
@@ -1,10 +1,20 @@
-//
-//  SKID_ApplicationFilters.h
-//  Skid
-//
-//  Created by Brad Allred on 12/3/11.
-//  Copyright (c) 2011 For Every Body. All rights reserved.
-//
+/* SKID - System Key Intercept and Dispatch
+ * Copyright (C) 2013 Brad Allred
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 #import <AppKit/NSRunningApplication.h>
 #import <Foundation/Foundation.h>
